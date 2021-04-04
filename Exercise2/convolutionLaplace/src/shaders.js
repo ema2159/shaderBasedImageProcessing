0a1ebefa4186fe37db2096b5c6335c5955b1c57a
Emmanuel Bustos <ema2159@gmail.com>
2021-04-04 14:49:24 +0200

    Add: Implement Laplacian filter

--- Exercise2/convolutionLaplace/src/shaders.js
+++ Exercise2/convolutionLaplace/src/shaders.js
@@ -14,36 +14,28 @@ uniform bool norm;
 
 varying vec2 vUv;
 
-#define PI 3.14159265358979323846
-#define PI2 6.28318530717958647692
+const int kernelSize = 3;
 
-// pos: position of pixel in the kernel
-// sigma: standard deviation of gaussian kernel
-float get_laplace_pix(vec2 pos, float sigma) {
-  return (1.0 - ((pow(pos.x, 2.0) + pow(pos.y, 2.0)) / (2.0 * pow(sigma, 2.0)))) *
-         exp(-((pow(pos.x, 2.0) + pow(pos.y, 2.0)) / (2.0 * pow(sigma, 2.0))));
-}
+const mat3 lapl_mat = mat3(-1.0, -1.0, -1.0,
+			   -1.0, 8.0, -1.0,
+			   -1.0, -1.0, -1.0);
 
 void main(void) {
   vec2 cellSize = 1.0 / resolution.xy;
   vec2 uv = vUv.xy;
 
-  vec4 textureValue = vec4(0, 0, 0, 0);
+  vec4 textureValue = vec4(0.0, 0.0, 0.0, 1.0);
   int kernelSizeDiv2 = kernelSize / 2;
 
-  float kernelSum = 0.0;
   for (int i = -kernelSizeDiv2; i <= kernelSizeDiv2; i++) {
     for (int j = -kernelSizeDiv2; j <= kernelSizeDiv2; j++) {
-      float pix_gauss_val = get_laplace_pix(vec2(float(i), float(j)), sigma);
-      kernelSum += pix_gauss_val;
-      textureValue +=
-          pix_gauss_val * texture2D(image, uv + vec2(float(i) * cellSize.x,
-                                                     float(j) * cellSize.y));
+      textureValue.rgb += lapl_mat[i + kernelSizeDiv2][j + kernelSizeDiv2] *
+                          texture2D(image, uv + vec2(float(i) * cellSize.x,
+                                                     float(j) * cellSize.y)).rgb;
     }
   }
 
-  textureValue /= kernelSum;
-  if(norm) {
+  if (norm) {
     textureValue = normalize(textureValue);
   }
   gl_FragColor = textureValue;
