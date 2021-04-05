8985dcc748ada7f12b5e2c62e46308e1398be1de
Emmanuel Bustos <ema2159@gmail.com>
2021-04-05 20:26:50 +0200

    Clean: Formatted fragment shader file

--- Exercise2/imageTransformation/src/shaders.js
+++ Exercise2/imageTransformation/src/shaders.js
@@ -36,16 +36,18 @@ vec3 bilinear_iterp(vec2 frag_pos, sampler2D image, vec2 cellSize) {
   vec3 pix2 = texture2D(image, vec2(h_1.x, h_0.y)).rgb;
   vec3 pix3 = texture2D(image, vec2(h_0.x, h_1.y)).rgb;
   vec3 pix4 = texture2D(image, vec2(h_1.x, h_1.y)).rgb;
-  vec3 horiz1 = mix(pix1, pix2, frag_pos.x); 
-  vec3 horiz2 = mix(pix3, pix4, frag_pos.x); 
+  vec3 horiz1 = mix(pix1, pix2, frag_pos.x);
+  vec3 horiz2 = mix(pix3, pix4, frag_pos.x);
   vec3 vert = mix(horiz1, horiz2, frag_pos.y);
   return vert;
 }
 
 vec3 nn_iterp(vec2 frag_pos, sampler2D image, vec2 cellSize) {
   vec2 pix_mod = mod(frag_pos, cellSize);
-  float h_coord = (pix_mod.x <= 0.5) ? frag_pos.x - pix_mod.x : frag_pos.x - pix_mod.x + cellSize.x;
-  float v_coord = (pix_mod.y <= 0.5) ? frag_pos.y - pix_mod.y : frag_pos.y - pix_mod.y + cellSize.y;
+  float h_coord = (pix_mod.x <= 0.5) ? frag_pos.x - pix_mod.x
+                                     : frag_pos.x - pix_mod.x + cellSize.x;
+  float v_coord = (pix_mod.y <= 0.5) ? frag_pos.y - pix_mod.y
+                                     : frag_pos.y - pix_mod.y + cellSize.y;
   vec3 pix = texture2D(image, vec2(h_coord, v_coord)).rgb;
   return pix;
 }
