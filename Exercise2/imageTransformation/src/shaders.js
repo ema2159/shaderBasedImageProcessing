e339ce38cf9613197068677a1ccf478d254906b3
Emmanuel Bustos <ema2159@gmail.com>
2021-04-05 19:58:16 +0200

    Add: Implement bilinear interpolation

--- Exercise2/imageTransformation/src/shaders.js
+++ Exercise2/imageTransformation/src/shaders.js
@@ -29,9 +29,19 @@ uniform float centerY;
 
 varying vec2 vUv;
 
-vec2 scale_coord(vec2 pt, float scale) {
-  mat2 scale_mat = mat2(scale, 0, 0, scale);
-  return scale_mat * pt;
+// Redundant. OpenGL does it by default.
+vec3 bilinear_iterp(vec2 frag_pos, sampler2D image, vec2 cellSize) {
+  vec2 h_0 = frag_pos + mod(frag_pos, cellSize);
+  vec2 h_1 = frag_pos + mod(frag_pos, cellSize) + cellSize;
+  vec3 pix1 = texture2D(image, vec2(h_0.x, h_0.y)).rgb;
+  vec3 pix2 = texture2D(image, vec2(h_1.x, h_0.y)).rgb;
+  vec3 pix3 = texture2D(image, vec2(h_0.x, h_1.y)).rgb;
+  vec3 pix4 = texture2D(image, vec2(h_1.x, h_1.y)).rgb;
+  vec3 horiz1 = mix(pix1, pix2, frag_pos.x); 
+  vec3 horiz2 = mix(pix3, pix4, frag_pos.x); 
+  vec3 vert = mix(horiz1, horiz2, frag_pos.y);
+  return vert;
+}
 }
 
 void main(void) {
