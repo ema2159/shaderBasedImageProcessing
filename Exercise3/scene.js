9f33c49efc324d8bbadf05fdda13cec8b75444da
Emmanuel Bustos <ema2159@gmail.com>
2021-04-08 19:20:24 +0200

    Mod: Adapt scene to the new IPGraph API

--- Exercise3/scene.js
+++ Exercise3/scene.js
@@ -1,50 +1,8 @@
 import * as THREE from "https://unpkg.com/three/build/three.module.js";
 import {OrbitControls} from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
 import {GUI} from "https://unpkg.com/three/examples/jsm/libs/dat.gui.module.js";
-import {vertexShader, fragmentShader} from "./shaders.js";
+import * as IPGraph from "./IPGraph.js";
 
-function IVimageProcessing(height, width, imageProcessingMaterial) {
-  this.height = height;
-  this.width = width;
-
-  //3 rtt setup
-  this.scene = new THREE.Scene();
-  // prettier-ignore
-  this.orthoCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 1 / Math.pow(2, 53), 1);
-
-  //4 create a target texture
-  var options = {
-    minFilter: THREE.NearestFilter,
-    magFilter: THREE.NearestFilter,
-    format: THREE.RGBAFormat,
-               // type:THREE.FloatType
-    type: THREE.UnsignedByteType,
-  };
-  this.rtt = new THREE.WebGLRenderTarget(width, height, options);
-
-  var geom = new THREE.BufferGeometry();
-  geom.addAttribute(
-    "position",
-    new THREE.BufferAttribute(
-      new Float32Array(
-        // prettier-ignore
-        [-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, -1, 0, 1, 1, 0, -1, 1, 0]
-      ),
-      3
-    )
-  );
-  geom.addAttribute(
-    "uv",
-    new THREE.BufferAttribute(
-      new Float32Array(
-        // prettier-ignore
-        [ 0,1, 1,1, 1,0, 0,1, 1,0, 0,0 ]
-      ),
-      2
-    )
-  );
-  this.scene.add(new THREE.Mesh(geom, imageProcessingMaterial));
-}
 
 function IVprocess(imageProcessing, renderer) {
   renderer.setRenderTarget(imageProcessing.rtt);
@@ -108,40 +66,25 @@ function init() {
     videoTexture.generateMipmaps = false;
     videoTexture.format = THREE.RGBFormat;
 
-    imageProcessingMaterial = new THREE.ShaderMaterial({
-      uniforms: {
-	// Image scaling
-        scaleX: {type: "f", value: 1.0},
-        scaleY: {type: "f", value: 1.0},
-        interpolation: {type: "i", value: 0},
-        image: {type: "t", value: videoTexture},
-        resolution: {
-          type: "2f",
-          value: new THREE.Vector2(video.videoWidth, video.videoHeight),
-        },
-      },
-      vertexShader: vertexShader,
-      fragmentShader: fragmentShader,
-      side: THREE.DoubleSide,
-    });
-
-    imageProcessing = new IVimageProcessing(
+    imageProcessing = new IPGraph.GaussFilter(
       video.videoHeight,
       video.videoWidth,
-      imageProcessingMaterial
+      videoTexture,
+      {
+	// sigma: {type: "f", value: 5.0},
+	// kernelSize: {type: "i", value: 31.0},
+      }
     );
-
-    console.log(imageProcessing.width);
 
     var geometry = new THREE.PlaneGeometry(
       1,
       video.videoHeight / video.videoWidth,
     );
     var material = new THREE.MeshBasicMaterial({
-      map: imageProcessing.rtt.texture,
+      map: videoTexture,
       side: THREE.DoubleSide,
     });
-    plane = new THREE.Mesh(geometry, imageProcessingMaterial);
+    plane = new THREE.Mesh(geometry, imageProcessing.material);
     plane.receiveShadow = false;
     plane.castShadow = false;
     scene.add(plane);
