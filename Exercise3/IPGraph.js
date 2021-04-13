97d849813f7ae59d6e0b160310bea52b52d6b650
Emmanuel Bustos <ema2159@gmail.com>
2021-04-13 11:00:44 +0200

    Doc: Added documentation for IPGraph's class and methods

--- Exercise3/IPGraph.js
+++ Exercise3/IPGraph.js
@@ -1,7 +1,11 @@
 /**
- * Abstract Class IPGraph.
+ * Class IPGraph.
  *
  * @class IPGraph
+ * This class implements an image procecssing graph over an input texture.
+ * @param {int} height       Height of the input texture.
+ * @param {int} width        Width of the input texture.
+ * @param {int} inputTexture Input texture.
  */
 class IPGraph {
   // Private class fields
@@ -21,7 +25,13 @@ class IPGraph {
     this.#subGraphs = [];
   }
 
-  // Node adding. Essentially a strategy pattern. 
+  /**
+   * Method for adding an image processing node at the end of the graph.
+   * Implemented utilizing an strategy design pattern.
+   * @param  {IPFilter} IPNode   Image processing filter to add to the graph.
+   * @param  {Object}   uniforms Object containing the uniforms to override default node uniforms.
+   * @return {IPGraph}  Implements a fluid API. Returns _this_.
+   */
   addNode(IPNode, uniforms) {
     let tex = this.#texture;
     let filterNode = new IPNode(this.#height, this.#width, tex, uniforms);
@@ -31,7 +41,12 @@ class IPGraph {
     return this;
   }
 
-  // Renderer initialization. Essentially an observer pattern.
+  /**
+   * Method for initializing graph's objects' offscreen renderer.
+   * It initializes each node in the graph and also the sub-graphs attached
+   * Implemented utilizing an observer design pattern.
+   * @param  {Object} renderer Scene's renderer.
+   */
   initializeRenderer(renderer) {
     // Initialize graph's nodes
     this.#nodes.forEach(node => node.initializeRenderer(renderer));
@@ -39,7 +54,13 @@ class IPGraph {
     this.#subGraphs.forEach(subGraph => subGraph.initializeRenderer(renderer));
   }
 
-  // Subscribe sub graphs for initialization
+  /**
+   * Method for subscribing a sub-graph to the current image processing graph.
+   * When the sub-graph is subscribed, its renderer will be initialized when the current graph's
+   * renderer is initialized.
+   * @param {Object} subGraph Sub-grahp to subscribe.
+   * @return {IPGraph}  Implements a fluid API. Returns _this_.
+   */
   subscribeSubGraph(subGraph) {
     this.#subGraphs.push(subGraph);
     return this;
