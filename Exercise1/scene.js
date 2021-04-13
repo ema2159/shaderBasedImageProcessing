648ad2fb1594a20a1dddd8cafbe9b3ddabb11bfc
Emmanuel Bustos <ema2159@gmail.com>
2021-04-13 20:35:22 +0200

    Mod: Decoupled video plane and geometry creation and added webcam/image support

--- Exercise1/scene.js
+++ Exercise1/scene.js
@@ -34,62 +34,63 @@ function init() {
 
   container.appendChild(renderer.domElement);
 
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.001,
-    10
+function createTexturePlanes(texture, height, width) {
+  let geometry = new THREE.PlaneGeometry(
+    1,
+    height / width,
   );
-  camera.position.z = 0.7;
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.minDistance = 0.005;
-  controls.maxDistance = 1.0;
-  controls.enableRotate = true;
-  controls.addEventListener("change", render);
-  controls.update();
+  let material = new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+  });
+  plane = new THREE.Mesh(geometry, material);
+  plane.position.x = 0.5;
+  plane.position.z = -0.5;
+  plane.rotation.y = -Math.PI/5;
+  plane.receiveShadow = false;
+  plane.castShadow = false;
+  scene.add(plane);
 
+  let geometry2 = new THREE.PlaneGeometry(
+    1,
+    height / width
+  );
+  let material2 = new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+  });
+  plane = new THREE.Mesh(geometry2, material2);
+  plane.position.x = -0.5;
+  plane.position.z = -0.5;
+  plane.rotation.y = Math.PI/5;
+  plane.receiveShadow = false;
+  plane.castShadow = false;
+  scene.add(plane);
+}
+
+function initializeVideo(webCam, source) {
   video = document.createElement("video");
-  video.src = "../../assets/video.mp4";
-  video.load();
-  video.muted = true;
-  video.loop = true;
+  if(webCam){
+    video.srcObject = source;
+    video.play();
+  } else {
+    video.src = source;
+    video.load();
+    video.muted = true;
+    video.loop = true;
+  }
 
   video.onloadeddata = function () {
     videoTexture = new THREE.VideoTexture(video);
-    videoTexture.wrapS = videoTexture.wrapT = THREE.RepeatWrapping;
+    // videoTexture.wrapS = videoTexture.wrapT = THREE.RepeatWrapping;
     videoTexture.minFilter = THREE.NearestFilter;
     videoTexture.magFilter = THREE.NearestFilter;
     videoTexture.generateMipmaps = false;
     videoTexture.format = THREE.RGBFormat;
 
-    var geometry = new THREE.PlaneGeometry(
-      1,
-      video.videoHeight / video.videoWidth,
-    );
-    var material = new THREE.MeshBasicMaterial({
-      map: videoTexture,
-      side: THREE.DoubleSide,
-    });
-    plane = new THREE.Mesh(geometry, material);
-    plane.receiveShadow = false;
-    plane.castShadow = false;
-    scene.add(plane);
+    createTexturePlanes(videoTexture, video.videoHeight, video.videoWidth);
 
-    var geometry2 = new THREE.PlaneGeometry(
-      1,
-      video.videoHeight / video.videoWidth
-    );
-    var material2 = new THREE.MeshBasicMaterial({
-      map: videoTexture,
-      side: THREE.DoubleSide,
-    });
-    plane = new THREE.Mesh(geometry2, material2);
-    plane.position.z = -0.5;
-    plane.receiveShadow = false;
-    plane.castShadow = false;
-    scene.add(plane);
-
-    var pausePlayObj = {
+    let pausePlayObj = {
       pausePlay: function () {
         if (!video.paused) {
           console.log("pause");
@@ -110,8 +111,51 @@ function init() {
     gui.add(pausePlayObj, "add10sec").name("Add 10 seconds");
     video.play();
   };
+}
 
-  window.addEventListener("resize", onWindowResize, false);
+function init() {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x13131C);
+
+  renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+  renderer.autoClear = false;
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = false;
+
+  container.appendChild(renderer.domElement);
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.001,
+    10
+  );
+  camera.position.z = 0.7;
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 0.005;
+  controls.maxDistance = 1.0;
+  controls.enableRotate = true;
+  controls.addEventListener("change", render);
+  controls.update();
+
+  // Add image processing planes
+  if(sourceImage==="webcam") {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia)  {
+      const constraints = {video: {width: 1280, height: 720, facingMode: 'user'}};
+      navigator.mediaDevices.getUserMedia(constraints).then(function (stream){
+	initializeVideo(true, stream);
+      })
+    };
+  } else if(sourceImage==="video") {
+    initializeVideo(false, "../../assets/video.mp4");
+  } else {
+    const imageTexture = new THREE.TextureLoader().load('../../assets/grenouille.jpg');
+    createTexturePlanes(imageTexture, window.innerHeight, window.innerWidth);
+  }
 }
 
 function render() {
@@ -131,3 +175,6 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
+
+// Readjust camera and renderer when window is resized
+window.addEventListener("resize", onWindowResize, false);
